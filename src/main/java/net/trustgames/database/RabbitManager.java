988e5f082ecadc9eb8cc0948036e34c379c4c01b
ThomasWega <tomous.wega@gmail.com>
2023-03-26 17:39:41 +0200

    test of delivery

--- src/main/java/net/trustgames/database/RabbitManager.java
+++ src/main/java/net/trustgames/database/RabbitManager.java
@@ -1,13 +1,14 @@
 package net.trustgames.database;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 public class RabbitManager {
 
@@ -62,4 +63,23 @@ public class RabbitManager {
             }
         });
     }
+
+    /**
+     * Handle the delivery of the message in the queue and everytime a
+     * message is recieved, a callback is run.
+     *
+     * @param callback Callback to be run everytime a message is received
+     * @throws IOException if an error occurred
+     */
+    public void delivery(Consumer<JSONObject> callback) throws IOException {
+        channel.basicConsume(queueName, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, com.rabbitmq.client.AMQP.BasicProperties properties, byte[] body) {
+                String fullMessage = new String(body, StandardCharsets.UTF_8);
+                JSONObject json = new JSONObject(fullMessage);
+                callback.accept(json);
+            }
+        });
+    }
+
 }
