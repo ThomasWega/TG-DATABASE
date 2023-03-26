bc62aff3105aba8275a71e73fc6e465d88f8d609
ThomasWega <tomous.wega@gmail.com>
2023-03-26 16:32:08 +0200

    test?

--- src/main/java/net/trustgames/database/Database.java
+++ src/main/java/net/trustgames/database/Database.java
@@ -6,8 +6,6 @@ import java.util.logging.Logger;
 
 public class Database {
 
-    // TODO add to Nexus Repo
-
     @Getter
     public static final Logger logger = Logger.getLogger("Database");
     public static void main(String[] args) {
